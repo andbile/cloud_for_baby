1230eb053036399d124c0dc3919f3eba56ac7151
Andrii Biletskyi <andbile@gmail.com>
2020-05-12 01:10:47 +0300

    Added adaptive for index-section-4

--- src/js/sliders.js
+++ src/js/sliders.js
@@ -2,7 +2,7 @@
 
 
 
-    // слайдер фото зданий в model_page.html
+    // слайдер - ОТЗЫВЫ О НАШЕЙ ПРОДУКЦИИ
     $('[data-slider-slick-01]').slick({
         speed: 1000,
         //autoplay: true,
@@ -12,6 +12,15 @@
         nextArrow: $('.slider-btn-wrp--reviews .slider-btn--next')
     });
 
-
+    // слайдер - идеи для фото
+    $('[data-slider-slick-02]').slick({
+        speed: 1000,
+        //autoplay: true,
+        //autoplaySpeed: 4000,
+        dots:false,
+        prevArrow: $('.index-section-4__slider .slider-btn--prev'),
+        nextArrow: $('.index-section-4__slider .slider-btn--next')
+    });
+2
 
 })();
