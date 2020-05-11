c8c877f15a0ee9b6a2d47cfd9c34e8ffd9d95f7d
Andrii Biletskyi <andbile@gmail.com>
2020-05-12 01:34:36 +0300

    Fix adaptive index-section-5

--- src/js/answer.js
+++ src/js/answer.js
@@ -2,9 +2,19 @@
 
     var $btns = $('.questions__plus');
     var $questionsItems = $('.questions__item');
+    var $questionsBlocks = $('.questions__block--question');
 
-    $btns.on('click', function () {
-        var $parent = $(this).closest('.questions__item');
+    $btns.on('click', function (evt) {
+        evt.stopPropagation();
+        showAnswer($(this));
+    });
+
+    $questionsBlocks.on('click', function () {
+        showAnswer($(this));
+    });
+
+    function showAnswer(target) {
+        var $parent = target.closest('.questions__item');
 
         if($parent.hasClass('active')){
             $parent.removeClass('active');
@@ -12,7 +22,7 @@
             $questionsItems.removeClass('active');
             $parent.addClass('active');
         }
-    });
+    }
 
 
     var $scrollContainer = $('.questions__items');
